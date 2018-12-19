d7179137d672db46cf9d8cb403063b0fb3524b62
StrangeKind <latuheru.g@gmail.com>
2018-12-19 14:54:11 +0100

    first setup

---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var cache = require('gulp-cache');
+var del = require('del');
+var autoprefixer = require('gulp-autoprefixer');
+var runSequence = require('run-sequence');
+var imagemin = require('gulp-imagemin');
+
+gulp.task('hello', function() {
+  console.log('Hello Goldwyn');
+});
+
+gulp.task('sass', function() {
+  return gulp.src('src/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('src/scss/**/*.scss', ['sass']);
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('./*.html', browserSync.reload);
+  gulp.watch('src/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    },
+  })
+})
+
+gulp.task('useref', function(){
+  return gulp.src('./*.html')
+    .pipe(useref())
+    // Minifies only if it's a JavaScript file
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('useref', function(){
+  return gulp.src('./*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('images', function(){
+  return gulp.src('src/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  // Caching images that ran through imagemin
+  .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('dist/src/img'))
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('src/fonts/**/*')
+  .pipe(gulp.dest('dist/src/fonts'))
+})
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
+
+gulp.task('autoprefixer', () =>
+    gulp.src('src/css/main.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('src/css'))
+);
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist',
+    ['sass', 'useref', 'images', 'fonts'],
+    callback
+  )
+})
